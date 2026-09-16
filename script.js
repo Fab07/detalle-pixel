f6c52618ee1dc2265225ff6f93e86a91ebc52c96
Fab07 <42525639+Fab07@users.noreply.github.com>
2026-09-15 21:58:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const carta = document.getElementById('carta');
 const textoClic = document.getElementById('texto-clic');
 
 contenedor.addEventListener('click', () => {
-    sobre.src = 'Sobre.png';
+    sobre.src = 'sobre.png';
     textoClic.style.display = 'none';
     carta.style.display = 'block';
 }, { once: true });
